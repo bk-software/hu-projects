f4a3733735dbf45cf86a488e3e18a5cb886a06bb
bk-software <barak.software.solutions@gmail.com>
2022-08-21 20:57:51 +0300

    add laps button

---
+++ stop-watch/step-06/stop-watch.js
@@ -0,0 +1,73 @@
+const stopWatchView = document.querySelector('#stop-watch-view');
+const toggleStopWatchButton = document.querySelector('#toggle-stop-watch');
+toggleStopWatchButton.addEventListener('click', toggleStopWatch);
+
+const resetButton = document.querySelector('#reset-stop-watch');
+resetButton.addEventListener('click', resetStopWatch);
+
+const lapsButton = document.querySelector('#stop-watch-laps');
+lapsButton.addEventListener('click', addLap);
+
+const lapsView = document.querySelector('#laps-view');
+
+let hundredth = 0;
+let seconds = 0;
+let minutes = 0;
+
+function displayTime() {
+  let seconsString = seconds;
+  if (seconds < 10) {
+    secondsString = '0' + seconds;
+  }
+  //TODO  do the same to the minutes
+  stopWatchView.innerHTML = `${minutes}:${secondsString}:${hundredth}`;
+}
+
+// counter = 5;
+// counter = counter + 1;
+// counter += 1;
+
+function addLap() {
+  lapsView.innerHTML += `<li>${minutes}:${seconds}:${hundredth}</li>`;
+}
+
+function resetStopWatch() {
+  hundredth = 0;
+  seconds = 0;
+  minutes = 0;
+  displayTime();
+}
+
+function handleTimeChange() {
+  hundredth++;
+  if (hundredth > 99) {
+    hundredth = 0;
+    seconds++;
+    if (seconds > 59) {
+      seconds = 0;
+      minutes++;
+    }
+  }
+
+  displayTime();
+}
+
+function startStopWatch() {
+  intervalId = setInterval(handleTimeChange, 10);
+  toggleStopWatchButton.innerHTML = 'Stop';
+}
+
+function stopStopWatch() {
+  clearInterval(intervalId);
+  toggleStopWatchButton.innerHTML = 'Start';
+  intervalId = null;
+}
+
+let intervalId = null;
+function toggleStopWatch() {
+  if (intervalId == null) {
+    startStopWatch();
+  } else {
+    stopStopWatch();
+  }
+}
