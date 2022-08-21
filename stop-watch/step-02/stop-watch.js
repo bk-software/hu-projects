5899c498a2f8e44f187fe32abb852b024634c380
bk-software <barak.software.solutions@gmail.com>
2022-08-21 18:55:07 +0300

    add disply time function

--- stop-watch/step-02/stop-watch.js
+++ stop-watch/step-02/stop-watch.js
@@ -1,1 +1,11 @@
 const stopWatchView = document.querySelector('#stop-watch-view');
+
+let hundredth = 44;
+let seconds = 34;
+let minutes = 12;
+
+function displayTime() {
+  stopWatchView.innerHTML = `${minutes}:${seconds}:${hundredth}`;
+}
+
+displayTime();
