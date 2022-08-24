1361beb0731a2a2283ec5728f2d6b14681ad76e8
bk-software <barak.software.solutions@gmail.com>
2022-08-24 17:52:29 +0300

    import export example

---
+++ timers/app.js
@@ -0,0 +1,4 @@
+import StamName, { name, number } from './timers.js';
+console.log('name', name);
+console.log('obj', StamName);
+console.log('number', number);
---
+++ timers/timers.js
@@ -0,0 +1,10 @@
+export const name = 'barak';
+
+export const number = 24;
+
+const obj = {
+  name: 'barak',
+  last_name: 'haviv',
+};
+
+export default obj;
