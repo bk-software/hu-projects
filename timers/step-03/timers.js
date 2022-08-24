62954e44aa1ad96e036cb821b842a97b2b8b0896
bk-software <barak.software.solutions@gmail.com>
2022-08-24 18:57:37 +0300

    add comments

--- timers/step-03/timers.js
+++ timers/step-03/timers.js
@@ -1,3 +1,4 @@
+// Constructor function use as a Template of creating objects
 function fullName(name, lastName) {
   this.name = name;
   this.last_name = lastName;
@@ -6,10 +7,16 @@ function fullName(name, lastName) {
   };
 }
 
+// create new object with fullName constructor function
 const obj1 = new fullName('Noam', 'Grin');
+// create another object
 const obj2 = new fullName('Tomer', 'Umar');
 
+// calling the fname function of the object
 obj1.fname();
 obj2.fname();
+
+console.log('obj1', obj1);
+console.log('obj2', obj2);
 
 export default fullName;
