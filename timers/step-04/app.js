b9faa64e218acfd47e5cb8a170a02e9ae9e96cbf
bk-software <barak.software.solutions@gmail.com>
2022-08-24 20:01:21 +0300

    create timer button create new timer

--- timers/step-04/app.js
+++ timers/step-04/app.js
@@ -1,12 +1,10 @@
 import Timer from './timers.js';
 
-// const t1 = new Timer(50);
-// console.log('timer 1', t1);
-
 document.querySelector('#create-timer').addEventListener('click', addTimer);
 const timerInput = document.querySelector('#time-input');
 
 function addTimer() {
   const initialTime = timerInput.value;
-  console.log('value from input', initialTime);
+  const timer = new Timer(initialTime);
+  console.log('timer', timer);
 }
--- timers/step-04/timers.js
+++ timers/step-04/timers.js
@@ -1,3 +1,4 @@
+//Constructor function
 function Timer(initialTime) {
   this.initialTime = initialTime;
 
