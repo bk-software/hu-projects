b4f90e36518273b342809b19a2aafa41ee886813
bk-software <barak.software.solutions@gmail.com>
2022-08-28 19:31:45 +0300

    add delete button

--- timers/step-08/timer.js
+++ timers/step-08/timer.js
@@ -3,10 +3,26 @@ export default function Timer(initialTime) {
   let time = initialTime;
   let timerElement;
 
+  function startTimer() {
+    console.log('timer is started');
+  }
+
+  function deleteTimer() {
+    console.log('delete timer function');
+  }
+
   function createStartButton() {
     const startButton = document.createElement('button');
     startButton.innerText = 'Start';
+    startButton.addEventListener('click', startTimer);
     timerElement.appendChild(startButton);
+  }
+
+  function createDeleteButton() {
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', deleteTimer);
+    timerElement.appendChild(deleteButton);
   }
 
   this.createTimer = function () {
@@ -16,6 +32,7 @@ export default function Timer(initialTime) {
     timerElement.innerText = time;
 
     createStartButton();
+    createDeleteButton();
 
     document.querySelector('#timers-container').appendChild(timerElement);
   };
