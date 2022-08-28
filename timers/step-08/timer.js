28bfb1c6ad744408d2b111da8805ae42f2035b91
bk-software <barak.software.solutions@gmail.com>
2022-08-28 18:57:12 +0300

    add createStartButton function in step 8

--- timers/step-08/timer.js
+++ timers/step-08/timer.js
@@ -1,12 +1,21 @@
 //timer.js
 export default function Timer(initialTime) {
   let time = initialTime;
+  let timerElement;
+
+  function createStartButton() {
+    const startButton = document.createElement('button');
+    startButton.innerText = 'Start';
+    timerElement.appendChild(startButton);
+  }
 
   this.createTimer = function () {
     console.log('create timer', time);
 
-    const timerElement = document.createElement('div');
+    timerElement = document.createElement('div');
     timerElement.innerText = time;
+
+    createStartButton();
 
     document.querySelector('#timers-container').appendChild(timerElement);
   };
